4cfa8c738de7893a1da303c1145ce6db028e4f78
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-06 12:00:18 +0100

    second test passes

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,24 +3,24 @@ import App from "./App";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Navbar from "./NavBar";
+import { Cards } from "./Cards";
 
 afterEach(rtl.cleanup);
-// beforeEach, beforeAll, afterAll
 
 it("shows that the word is present ", () => {
-  // step 3 - mount the component we want to test
   const wrapper = rtl.render(<Navbar />);
   wrapper.debug();
-  // step 4 - capture the element of interest BY "READABLE" PROPERTY
-  // we normally care about the readable text
-  // or the aria attributes (screen readers)
-  const learnReactElement = wrapper.queryByText(/players/i);
 
-  // const dis = wrapper.queryByText(/display/i)
+  const checkPlayers = wrapper.queryByText(/players/i);
 
-  // step 5 - assert that the element is there
-  expect(learnReactElement).toBeInTheDocument();
+  expect(checkPlayers).toBeInTheDocument();
+});
 
-  // const linkElement = getByText(/2/i); // 2 because we clicked twice
-  // expect(linkElement).toBeInTheDocument()
+it("shows the length of players array", () => {
+  const wrapper = rtl.render(<App/>);
+  wrapper.debug();
+
+  const checkName = wrapper.queryByText(/world/i);
+
+  expect(checkName).toBeInTheDocument();
 });
