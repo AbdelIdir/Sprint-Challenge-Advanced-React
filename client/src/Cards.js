063969c5933d42180e90950f42a1e5f746f76382
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-06 13:31:24 +0100

    added additional styling

--- client/src/Cards.js
+++ client/src/Cards.js
@@ -4,8 +4,13 @@ import "./App.css";
 export const Cards = props => {
   return (
     <div className="card">
-      <p>name: {props.player.name} </p>
+      <b>
+        <p style={{ textDecoration: "underline" }}>
+          name: {props.player.name}{" "}
+        </p>
+      </b>
       <p>country :{props.player.country} </p>
+      <p>Searches:{props.player.searches}</p>
     </div>
   );
 };
