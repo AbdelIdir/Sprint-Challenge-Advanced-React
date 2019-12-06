24d4c161f157affcedc5c7f6a4b44544e9acb225
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-06 10:10:36 +0100

    api data successfully fetched

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      hmm
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          players: response.data
+        });
+      })
+
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
